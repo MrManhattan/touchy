6d0658c77ccf1023558394c65b94fb854b746507
Hampus Ahlgren <wcw106@gmail.com>
2015-01-30 12:47:21 +0000

    Updated layout

--- wear/src/main/java/com/app/poke/poke/MainActivityWear.java
+++ wear/src/main/java/com/app/poke/poke/MainActivityWear.java
@@ -3,6 +3,8 @@ package com.app.poke.poke;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivityWear extends Activity {
@@ -14,10 +16,19 @@ public class MainActivityWear extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_wear);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                Button btnRecord = (Button) stub.findViewById(R.id.btnRecord);
+
+                btnRecord.setOnClickListener(new WatchViewStub.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        mTextView.setText("Guling guling kom nu lilla fuling!");
+                    }
+                });
             }
         });
 

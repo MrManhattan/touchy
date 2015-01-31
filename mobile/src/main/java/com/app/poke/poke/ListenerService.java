79504cfd6923a64c530faa752d8d135d987ab302
Staffan Piledahl <staefy@gmail.com>
2015-01-31 20:15:12 +0000

    ListenerService.java

---
+++ mobile/src/main/java/com/app/poke/poke/ListenerService.java
@@ -0,0 +1,38 @@
+package com.app.poke.poke;
+
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by staefy on 31/01/15.
+ */
+    public class ListenerService extends WearableListenerService {
+
+    String nodeId;
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        nodeId = messageEvent.getSourceNodeId();
+        showToast(messageEvent.getPath());
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+
+    private void reply(String message) {
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        client.blockingConnect(3000, TimeUnit.MILLISECONDS);
+        Wearable.MessageApi.sendMessage(client, nodeId, message, null);
+        client.disconnect();
+    }
+}

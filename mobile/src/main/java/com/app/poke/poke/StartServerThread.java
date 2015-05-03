3563bbaf59de2609bfbe3ff8cd81c031c05baa67
Hampus Ahlgren <wcw106@gmail.com>
2015-05-03 18:45:17 +0100

    Reading socket and saving to a file

--- mobile/src/main/java/com/app/poke/poke/StartServerThread.java
+++ mobile/src/main/java/com/app/poke/poke/StartServerThread.java
@@ -1,8 +1,13 @@
 package com.app.poke.poke;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -68,15 +73,40 @@ class StartServerThread implements Runnable {
         }
 
         public void run(){
-            BufferedReader br = new BufferedReader(new InputStreamReader(this.in));
+
 
             while(true){
                 try{
-                    String line = br.readLine();
-                    System.out.println("Server: Response from client: "+line);
+                   while(this.in.available() >= 0){
+                    readFile(this.in);
+                   }
+
                 }catch(Exception e){
                     System.out.println(e.toString());
                 }
+            }
+        }
+
+        public void readFile(InputStream inputStream){
+
+            OutputStream outputStream = null;
+
+            try {
+
+                // write the inputStream to a FileOutputStream
+                outputStream = new FileOutputStream(new File("audio.mp3"));
+
+                int read = 0;
+                byte[] bytes = new byte[1024];
+
+                while ((read = inputStream.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, read);
+                }
+
+                System.out.println("Done!");
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
